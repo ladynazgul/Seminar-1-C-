2346a8b91d837eb8cb8d10ce8e0e1c904164c4af
ladynazgool <ladynazgool@gmail.com>
2022-08-16 16:39:01 +0200

    Task-03 added

---
+++ Task-03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введите первое число: ");
+string A = Console.ReadLine();
+int a = int.Parse(A);
+Console.Write("Введите второе число: ");
+string B = Console.ReadLine();
+int b = int.Parse(B);
+Console.Write("Введите третье число: ");
+string C = Console.ReadLine();
+int c = int.Parse(C);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+// В таком варианте нужно после каждого числа нажимать "ентер", иначе программа зависает. Мой изначальный вариант был лучше.
