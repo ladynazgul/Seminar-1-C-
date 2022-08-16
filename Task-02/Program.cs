ae0322f163c6ac57ef1bac9c932eae175f6d5d1a
ladynazgool <ladynazgool@gmail.com>
2022-08-16 09:19:10 +0200

    Task-02 two variants added

---
+++ Task-02/Program.cs
@@ -0,0 +1,42 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число: ");
+string numA = Console.ReadLine();
+int a = int.Parse(numA);
+Console.Write("Введите второе число: ");
+string numB = Console.ReadLine();
+int b = int.Parse(numB);
+
+if (a > b)
+{
+    Console.WriteLine($"Число {a} больше чем {b}");
+}
+else
+{
+    Console.WriteLine($"Число {b} больше чем {a}");
+}
+
+// Вариант Юли, закоменчу его для примера
+// Console.Write("Введите первое число: ");
+// string numA = Console.ReadLine();
+// int number1 = int.Parse(numA);
+// Console.Write("Введите второе число: ");
+// string numB = Console.ReadLine();
+// int number2 = int.Parse(numB);
+
+// int max = number1;
+// int min = number2;
+
+// if (number1 > number2)
+// {
+//     max = number1;
+//     min = number2;
+// }
+// else
+// {
+//   min = number1;
+//    max = number2;
+// }
+
+// Console.WriteLine(min);
+// Console.WriteLine(max);
