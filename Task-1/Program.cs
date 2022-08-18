8cf6d60260b6f25d3bd2a299e0c09513eaa9af7c
ladynazgool <ladynazgool@gmail.com>
2022-08-18 10:57:02 +0200

    Small approvements in Task-1

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// По двум заданным числам проверить, является ли первое квадратом второго
 Console.Write("Введите первое число: ");
-string numA = Console.ReadLine();
+string numA = Console.ReadLine() ?? "0";
 int a = int.Parse(numA);
 Console.Write("Введите второе число: ");
-string numB = Console.ReadLine();
+string numB = Console.ReadLine() ?? "0";
 int b = int.Parse(numB);
 
 if (a == b * b)
