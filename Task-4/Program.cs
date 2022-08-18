fc5172d39ae9010bb84a78082f667e6e5468294a
ladynazgool <ladynazgool@gmail.com>
2022-08-18 19:44:41 +0200

    Task-4 added

---
+++ Task-4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трехзначного числа
+
+int number = new Random().Next(100, 1000);
+int a = number % 10;
+Console.WriteLine("Число: " + number);
+Console.WriteLine("Последняя цифра: " + a);
+Console.ReadKey(true);
