f6cbb45e8a3f638a0f1b456e0ccf9b5774f4bdc8
ladynazgool <ladynazgool@gmail.com>
2022-08-18 21:19:54 +0200

    Task-5 added

---
+++ Task-5/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+int a = (number % 100 - number % 10) / 10;
+Console.WriteLine("Число: " + number);
+Console.WriteLine("Вторая цифра: " + a);
+Console.ReadKey(true);
