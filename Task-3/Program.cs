3a1836cd91feb92be5410ea86678ff73cbe3cd84
ladynazgool <ladynazgool@gmail.com>
2022-08-18 16:36:04 +0200

    Task-3 added

---
+++ Task-3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = -n; i <= n; i++)
+{
+    Console.WriteLine(i);
+}
+Console.ReadKey();
