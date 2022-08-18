ebed6bc6bb64beee29af16443fdf2a58197c7700
ladynazgool <ladynazgool@gmail.com>
2022-08-18 09:52:02 +0200

    Task-05 added

---
+++ Task-05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.WriteLine(i + " ");
+    }
+}
+Console.ReadKey();
