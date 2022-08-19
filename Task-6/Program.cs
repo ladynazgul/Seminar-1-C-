25452819aca34bf61a630bcba336996201bebbf2
ladynazgool <ladynazgool@gmail.com>
2022-08-19 08:47:31 +0200

    Task-6 added

---
+++ Task-6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int x = new Random().Next(10, 100);
+Console.WriteLine($"Дано число {x}");
+string s = x.ToString();
+
+int a = int.Parse(s[0].ToString());
+int b = int.Parse(s[1].ToString());
+
+if (a>b)
+{
+    Console.WriteLine($"Наибольшая цифра {a}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра {b}");
+}
