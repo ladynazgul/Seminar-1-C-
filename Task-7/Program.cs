83395841e55780fba861af693e24bd3969cc3f72
ladynazgool <ladynazgool@gmail.com>
2022-08-19 16:13:08 +0200

    Task-7 added

---
+++ Task-7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+System.Console.WriteLine("Введите трехзначное число: ");
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int NewNumber = 0;
+
+int NumberWithoutSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumberWithoutSecDig();
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+System.Console.WriteLine();
