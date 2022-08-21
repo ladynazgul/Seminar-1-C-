741129d4705068b5eb9eacea7b8945f74a2ff73b
ladynazgool <ladynazgool@gmail.com>
2022-08-21 04:49:45 +0200

    Task-8 added

---
+++ Task-8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите заданное число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+if (a % b == 0)
+{
+    Console.WriteLine($"Число {a} кратно числу {b}");
+}
+
+else
+{
+    Console.WriteLine($"Остаток равен {a % b}");
+}
