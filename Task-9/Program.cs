08b8bc3058815a36f9e7e8fdf4888f72f350c8f2
ladynazgool <ladynazgool@gmail.com>
2022-08-21 14:44:40 +0200

    Task-9 added

---
+++ Task-9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+if (a / 100 > 0)
+{
+    Console.WriteLine($"Третьей цифрой числа {a} является {a % 10}");
+}
+else
+{
+    Console.WriteLine($"У числа {a} нет третьей цифры");
+}
